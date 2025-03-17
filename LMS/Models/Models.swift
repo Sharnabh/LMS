5d2067bfe348726779f0be1b64eae02337acb7af
Sharnabh Banerjee <banerjeesharnabh@gmail.com>
2025-03-17 22:53:09 +0530

    Added Admin Model

---
+++ LMS/Models/Models.swift
@@ -0,0 +1,15 @@
+//
+//  Models.swift
+//  LMS
+//
+//  Created by Sharnabh on 17/03/25.
+//
+
+import Foundation
+
+struct Admin {
+    let id: UUID
+    let adminId: String
+    let email: String
+    let password: String
+}

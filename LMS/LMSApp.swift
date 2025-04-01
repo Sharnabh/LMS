044b1cda526c393a28b407184dcd14d23fddd910
UtkarshSaxena812 <utkarshrajsaxena8@gmail.com>
2025-04-01 22:51:27 +0530

    changes in entire UI (Admin)

--- LMS/LMSApp.swift
+++ LMS/LMSApp.swift
@@ -9,9 +9,35 @@ import SwiftUI
 
 @main
 struct LMSApp: App {
+    @StateObject private var appState = AppState()
+    @AppStorage("hasLaunchedBefore") private var hasLaunchedBefore = false
+    @AppStorage("adminIsLoggedIn") private var adminIsLoggedIn = false
+    @AppStorage("studentIsLoggedIn") private var studentIsLoggedIn = false
+    @AppStorage("librarianIsLoggedIn") private var librarianIsLoggedIn = false
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            if !hasLaunchedBefore {
+                WelcomeScreenView()
+                    .environmentObject(appState)
+                    .onAppear {
+                        // Set hasLaunchedBefore to true so it only shows once
+                        hasLaunchedBefore = true
+                    }
+            } else if adminIsLoggedIn {
+                MainAppView(userRole: .admin, initialTab: 0)
+                    .environmentObject(appState)
+            } else if librarianIsLoggedIn {
+                MainAppView(userRole: .librarian, initialTab: 0)
+                    .environmentObject(appState)
+            } else if studentIsLoggedIn {
+                MainAppView(userRole: .member, initialTab: 0)
+                    .environmentObject(appState)
+            } else {
+                // User is not logged in, show login screen
+                WelcomeScreenView()
+                    .environmentObject(appState)
+            }
         }
     }
 }

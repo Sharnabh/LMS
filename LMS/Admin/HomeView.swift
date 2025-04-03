aedfc156bce60732485351fe04ecb8018fe5627a
SINGHKING5 <dipanshusingh011003@gmail.com>
2025-04-03 18:42:27 +0530

    Space between home and dashboard

--- LMS/Admin/HomeView.swift
+++ LMS/Admin/HomeView.swift
@@ -60,8 +60,7 @@ struct HomeView: View {
     var body: some View {
         NavigationStack {
             ScrollView {
-                VStack(alignment: .leading, spacing: 20) {
-                    Spacer()
+                VStack(alignment: .leading, spacing: 16) {
                     // Dashboard Header
                     Text("Dashboard")
                         .font(.title2)
